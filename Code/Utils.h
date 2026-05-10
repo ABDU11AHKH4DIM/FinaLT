2ae1e0fa447ab2e2cd8c44ff2caeaaf8054362cc
ABDU11AHKH4DIM <abdullahkhadim4700@gmail.com>
2026-05-10 13:00:49 +0500

    Update cctype include for tolower and toupper

--- Code/Utils.h
+++ Code/Utils.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <string>			// for std::string
-#include <cctype>			// for tolower()
+#include <cctype>			// for tolower() and toupper()
 
 // commenting out cctype and limits headers won't give compile errors, but they are used in this program. This might be because of 'Transitive includes'.
 
 // ============================================================
 // Utils
 // ============================================================
-
+std::string toupperString(std::string str);
 std::string tolowerString(std::string str);
